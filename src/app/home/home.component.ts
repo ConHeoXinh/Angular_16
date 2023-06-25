fd9c4dbcd61470d90e5c44e1b26270a70df28a60
ConHeoXinh <quanthienlong21@gmail.com>
2023-06-25 16:16:13 +0700

    For if else....

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   public name = 'Long';
   public age = 15;
+  public traiCay = ['Tao', 'Nho'];
+  public traiCay2 = [{ ten: 'Tao', gia: 12 }, { ten: 'Nho', gia: 20 }];
 
   constructor() {
 
+  }
+  public ngOnInit(): void {
+    console.log('trai cay =', this.traiCay);
   }
 
   public resetName(): void {
